2f3b40ee58d43024ec102b07c7d5e4c8d422add7
Gerry Weißbach <gerry.w@gammaproduction.de>
2014-02-12 13:27:15 +0100

    Added more options (domainName and anonymize)

--- action.php
+++ action.php
@@ -4,20 +4,6 @@ if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
 require_once(DOKU_PLUGIN.'action.php');
 
 class action_plugin_googleanalytics extends DokuWiki_Action_Plugin {
-
-	/**
-	 * return some info
-	 */
-	function getInfo(){
-		return array(
-			'author' => 'Terence J. Grant',
-			'email'  => 'tjgrant@tatewake.com',
-			'date'   => '2013-02-23',
-			'name'   => 'Google Analytics Plugin',
-			'desc'   => 'Plugin to embed your google analytics code for your site.',
-			'url'    => 'http://tatewake.com/wiki/projects:google_analytics_for_dokuwiki',
-		);
-	}
 	
 	/**
 	 * Register its handlers with the DokuWiki's event controller
@@ -31,10 +17,26 @@ class action_plugin_googleanalytics extends DokuWiki_Action_Plugin {
 		if(!$this->getConf('GAID')) return;
 		if($this->getConf('dont_count_admin') && $INFO['isadmin']) return;
 		if($this->getConf('dont_count_users') && $_SERVER['REMOTE_USER']) return;
+		
+		$options = array();
+		$options[] = "_gaq.push(['_setAccount', '".$this->getConf('GAID')."'])";
+
+		if ( $this->getConf('anonymize') ) {
+			$options[] = "_gaq.push(['_gat._anonymizeIp'])";
+		}
+		
+		$domainName = $this->getConf('domainName');
+		if ( !empty($domainName) ) {
+			$options[] = "_gaq.push(['_setDomainName', '".$domainName."']);";
+		}
+		
+		$options[] = "_gaq.push(['_trackPageview'])";
+
 		$event->data["script"][] = array (
 		  "type" => "text/javascript",
-		  "_data" => "var _gaq = _gaq || []; _gaq.push(['_setAccount', '".$this->getConf('GAID')."']); _gaq.push(['_trackPageview']);"
+		  "_data" => "var _gaq = _gaq || [];" . implode(';', $options)
 		);
+		
 		$event->data["script"][] = array (
 		  "type" => "text/javascript",
 		  "_data" => "(function(){var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true; ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s); })();"
@@ -42,4 +44,3 @@ class action_plugin_googleanalytics extends DokuWiki_Action_Plugin {
 
 	}
 }
-?>
