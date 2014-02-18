cc9f5943498ef17e0f0d6cfebb9c4a6ba58cf624
Gerry Weißbach <gerry.w@gammaproduction.de>
2014-02-18 14:53:41 +0100

    Only add Script if we don't run via AJAX.

--- action.php
+++ action.php
@@ -4,17 +4,26 @@ if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
 require_once(DOKU_PLUGIN.'action.php');
 
 class action_plugin_googleanalytics extends DokuWiki_Action_Plugin {
+
+	private $mustNotShow = false;
 	
 	/**
 	 * Register its handlers with the DokuWiki's event controller
 	 */
 	function register(&$controller) {
 	    $controller->register_hook('TPL_METAHEADER_OUTPUT', 'BEFORE',  $this, '_addHeaders');
+        $controller->register_hook('AJAX_CALL_UNKNOWN', 'BEFORE', $this, 'ajax_provider');
+	}
+	
+	function ajax_provider(&$event) {
+		
+		global $ACT;
+		$this->mustNotShow = $ACT != 'show';
 	}
 
 	function _addHeaders (&$event, $param) {
 		global $INFO;
-		if(!$this->getConf('GAID')) return;
+		if($this->mustNotShow || !$this->getConf('GAID')) return;
 		if($this->getConf('dont_count_admin') && $INFO['isadmin']) return;
 		if($this->getConf('dont_count_users') && $_SERVER['REMOTE_USER']) return;
 		
