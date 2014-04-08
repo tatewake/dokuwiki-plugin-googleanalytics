98a5b990b0f2689f26d110f95e49ab18ab914b75
Gerry Weißbach <gerry.w@gammaproduction.de>
2014-04-08 16:12:08 +0200

    Added event for popupviewer to track its links

--- action.php
+++ action.php
@@ -11,45 +11,59 @@ class action_plugin_googleanalytics extends DokuWiki_Action_Plugin {
 	 * Register its handlers with the DokuWiki's event controller
 	 */
 	function register(&$controller) {
+            $controller->register_hook('POPUPVIEWER_DOKUWIKI_STARTED', 'BEFORE', $this, 'popupviewer_handler');
 	    $controller->register_hook('TPL_METAHEADER_OUTPUT', 'BEFORE',  $this, '_addHeaders');
-        $controller->register_hook('AJAX_CALL_UNKNOWN', 'BEFORE', $this, 'ajax_provider');
+            $controller->register_hook('AJAX_CALL_UNKNOWN', 'BEFORE', $this, 'ajax_provider');
+	}
+	
+	function popupviewer_handler(&$event) {
+	
+	    global $ID;
+	
+	    // Only track self
+            $event->data["popupscript"][] = array (
+                "type" => "text/popupscript",
+                "_data" => "googleanalytics_trackLink(typeof trackLink == 'string'?trackLink:'".wl($ID)."');"
+            );
+	
+    	    $this->mustNotShow = true;
 	}
 	
 	function ajax_provider(&$event) {
 		
-		global $ACT;
-		$this->mustNotShow = $ACT != 'show';
+            global $ACT;
+            $this->mustNotShow = $ACT != 'show';
 	}
 
 	function _addHeaders (&$event, $param) {
-		global $INFO;
-		if($this->mustNotShow || !$this->getConf('GAID')) return;
-		if($this->getConf('dont_count_admin') && $INFO['isadmin']) return;
-		if($this->getConf('dont_count_users') && $_SERVER['REMOTE_USER']) return;
+            global $INFO;
+            if($this->mustNotShow || !$this->getConf('GAID')) return;
+            if($this->getConf('dont_count_admin') && $INFO['isadmin']) return;
+            if($this->getConf('dont_count_users') && $_SERVER['REMOTE_USER']) return;
 		
-		$options = array();
-		$options[] = "_gaq.push(['_setAccount', '".$this->getConf('GAID')."'])";
+            $options = array();
+            $options[] = "_gaq.push(['_setAccount', '".$this->getConf('GAID')."'])";
 
-		if ( $this->getConf('anonymize') ) {
-			$options[] = "_gaq.push(['_gat._anonymizeIp'])";
-		}
+            if ( $this->getConf('anonymize') ) {
+                $options[] = "_gaq.push(['_gat._anonymizeIp'])";
+            }
 		
-		$domainName = $this->getConf('domainName');
-		if ( !empty($domainName) ) {
-			$options[] = "_gaq.push(['_setDomainName', '".$domainName."']);";
-		}
+            $domainName = $this->getConf('domainName');
+            if ( !empty($domainName) ) {
+                $options[] = "_gaq.push(['_setDomainName', '".$domainName."'])";
+            }
 		
-		$options[] = "_gaq.push(['_trackPageview'])";
+            $options[] = "_gaq.push(['_gat._forceSSL'])";
+            $options[] = "_gaq.push(['_trackPageview']);";
 
-		$event->data["script"][] = array (
-		  "type" => "text/javascript",
-		  "_data" => "var _gaq = _gaq || [];" . implode(';', $options)
-		);
+            $event->data["script"][] = array (
+                "type" => "text/javascript",
+                "_data" => "var _gaq = _gaq || [];" . implode(';', $options)
+            );
 		
-		$event->data["script"][] = array (
-		  "type" => "text/javascript",
-		  "_data" => "(function(){var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true; ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s); })();"
-		);
-
+            $event->data["script"][] = array (
+                "type" => "text/javascript",
+                "_data" => "(function() {var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true; ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s); })();"
+            );
 	}
 }
