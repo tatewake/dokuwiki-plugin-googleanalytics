f699e419b82e5e4da2eec080b8a3ddcde7051eee
Kacper <noreply@kacper.se>
2014-06-14 22:10:59 +0200

    Added Polish and Swedish translations

---
+++ lang/pl/settings.php
@@ -0,0 +1,4 @@
+<?php
+$lang['GAID'] = 'Google Analitycs ID';
+$lang['dont_count_admin'] = 'Nie zliczaj: admin/superuser';
+$lang['dont_count_users'] = 'Nie zliczaj zalogownych użytkowników';
---
+++ lang/se/settings.php
@@ -0,0 +1,4 @@
+<?php
+$lang['GAID'] = 'Google Analitycs ID';
+$lang['dont_count_admin'] = 'Räkna inte admin/superuser';
+$lang['dont_count_users'] = 'Räkna inte inloggade användare';
