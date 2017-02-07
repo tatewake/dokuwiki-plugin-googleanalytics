fd0d2f7d9f30e21a194af5b8eb8d89e28ff97d1c
Andreas Gohr <andi@splitbrain.org>
2017-02-07 19:31:48 +0100

    some cleanup for the action.php file

--- action.php
+++ action.php
@@ -1,69 +1,92 @@
 <?php
 if(!defined('DOKU_INC')) die();
-if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
-require_once(DOKU_PLUGIN.'action.php');
+if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN', DOKU_INC . 'lib/plugins/');
 
+/**
+ * Class action_plugin_googleanalytics
+ */
 class action_plugin_googleanalytics extends DokuWiki_Action_Plugin {
 
-	private $mustNotShow = false;
+    private $mustNotShow = false;
 
-	/**
-	 * Register its handlers with the DokuWiki's event controller
-	 */
-	function register(Doku_Event_Handler $controller) {
-            $controller->register_hook('POPUPVIEWER_DOKUWIKI_STARTED', 'BEFORE', $this, 'popupviewer_handler');
-	    $controller->register_hook('TPL_METAHEADER_OUTPUT', 'BEFORE',  $this, '_addHeaders');
-            $controller->register_hook('AJAX_CALL_UNKNOWN', 'BEFORE', $this, 'ajax_provider');
-	}
+    /**
+     * Register its handlers with the DokuWiki's event controller
+     *
+     * @param Doku_Event_Handler $controller
+     */
+    function register(Doku_Event_Handler $controller) {
+        $controller->register_hook('TPL_METAHEADER_OUTPUT', 'BEFORE', $this, '_addHeaders');
+        $controller->register_hook('POPUPVIEWER_DOKUWIKI_STARTED', 'BEFORE', $this, 'popupviewer_handler');
+        $controller->register_hook('AJAX_CALL_UNKNOWN', 'BEFORE', $this, 'ajax_provider');
+    }
 
-	function popupviewer_handler(&$event) {
+    /**
+     * Default setup of the needed javascript for tracking
+     *
+     * @param Doku_Event $event
+     * @param array $param
+     */
+    function _addHeaders(Doku_Event $event, $param) {
+        global $INFO;
+        if($this->mustNotShow || !$this->getConf('GAID')) return;
+        if($this->getConf('dont_count_admin') && $INFO['isadmin']) return;
+        if($this->getConf('dont_count_users') && $_SERVER['REMOTE_USER']) return;
 
-	    global $ID;
+        $options = array();
+        $options[] = "_gaq.push(['_setAccount', '" . $this->getConf('GAID') . "'])";
 
-	    // Only track self
-            $event->data["popupscript"][] = array (
-                "type" => "text/popupscript",
-                "_data" => "googleanalytics_trackLink(typeof trackLink == 'string'?trackLink:'".wl($ID)."');"
-            );
+        if($this->getConf('anonymize')) {
+            $options[] = "_gaq.push(['_gat._anonymizeIp'])";
+        }
 
-    	    $this->mustNotShow = true;
-	}
+        $domainName = $this->getConf('domainName');
+        if(!empty($domainName)) {
+            $options[] = "_gaq.push(['_setDomainName', '" . $domainName . "'])";
+        }
 
-	function ajax_provider(&$event) {
+        $options[] = "_gaq.push(['_gat._forceSSL'])";
+        $options[] = "_gaq.push(['_trackPageview']);";
 
-            global $ACT;
-            $this->mustNotShow = $ACT != 'show';
-	}
+        $event->data["script"][] = array(
+            "type" => "text/javascript",
+            "_data" => "var _gaq = _gaq || [];" . implode(';', $options)
+        );
 
-	function _addHeaders (&$event, $param) {
-            global $INFO;
-            if($this->mustNotShow || !$this->getConf('GAID')) return;
-            if($this->getConf('dont_count_admin') && $INFO['isadmin']) return;
-            if($this->getConf('dont_count_users') && $_SERVER['REMOTE_USER']) return;
+        $event->data["script"][] = array(
+            "type" => "text/javascript",
+            "_data" => "(function() {var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true; ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s); })();"
+        );
+    }
 
-            $options = array();
-            $options[] = "_gaq.push(['_setAccount', '".$this->getConf('GAID')."'])";
+    /**
+     * Track links in the popuviewer plugin
+     *
+     * @link https://www.dokuwiki.org/plugin:popupviewer
+     * @param Doku_Event $event
+     * @param array $param
+     */
+    function popupviewer_handler(Doku_Event $event, $param) {
+        global $ID;
 
-            if ( $this->getConf('anonymize') ) {
-                $options[] = "_gaq.push(['_gat._anonymizeIp'])";
-            }
+        // Only track self
+        $event->data["popupscript"][] = array(
+            "type" => "text/popupscript",
+            "_data" => "googleanalytics_trackLink(typeof trackLink == 'string'?trackLink:'" . wl($ID) . "');"
+        );
 
-            $domainName = $this->getConf('domainName');
-            if ( !empty($domainName) ) {
-                $options[] = "_gaq.push(['_setDomainName', '".$domainName."'])";
-            }
+        $this->mustNotShow = true;
+    }
 
-            $options[] = "_gaq.push(['_gat._forceSSL'])";
-            $options[] = "_gaq.push(['_trackPageview']);";
+    /**
+     * Handle page tracking when pages are loaded via AJAX
+     *
+     * @link https://www.dokuwiki.org/plugin:fastwiki
+     * @param Doku_Event $event
+     * @param array $param
+     */
+    function ajax_provider(Doku_Event $event, $param) {
+        global $ACT;
+        $this->mustNotShow = $ACT != 'show';
+    }
 
-            $event->data["script"][] = array (
-                "type" => "text/javascript",
-                "_data" => "var _gaq = _gaq || [];" . implode(';', $options)
-            );
-
-            $event->data["script"][] = array (
-                "type" => "text/javascript",
-                "_data" => "(function() {var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true; ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s); })();"
-            );
-	}
 }
