8d83c8a07d0d00b0acf3a4ffb74b141d78ac2379
Andreas Gohr <andi@splitbrain.org>
2017-02-08 19:07:02 +0100

    keep the query string (after some cleanup)

--- action.php
+++ action.php
@@ -28,15 +28,13 @@ class action_plugin_googleanalytics extends DokuWiki_Action_Plugin {
         global $JSINFO;
         global $INFO;
         global $ACT;
-        global $QUERY;
-        global $ID;
-        global $INPUT;
 
         if(!$this->gaEnabled) return;
         $trackingId = $this->getConf('GAID');
         if(!$trackingId) return;
         if($this->getConf('dont_count_admin') && $INFO['isadmin']) return;
         if($this->getConf('dont_count_users') && $_SERVER['REMOTE_USER']) return;
+        act_clean($ACT);
 
         $options = array();
         if($this->getConf('track_users') && $_SERVER['REMOTE_USER']) {
@@ -47,25 +45,52 @@ class action_plugin_googleanalytics extends DokuWiki_Action_Plugin {
             $options['legacyCookieDomain'] = $this->getConf('domainName');
         }
 
-        // normalize the pageview
+        $JSINFO['ga'] = array(
+            'trackingId' => $trackingId,
+            'anonymizeIp' => (bool) $this->getConf('anonymize'),
+            'action' => $ACT,
+            'trackOutboundLinks' => (bool) $this->getConf('track_links'),
+            'options' => $options,
+            'pageview' => $this->getPageView(),
+        );
+    }
+
+    /**
+     * normalize the pageview
+     *
+     * @return string
+     */
+    protected function getPageView() {
+        global $QUERY;
+        global $ID;
+        global $INPUT;
+        global $ACT;
+
+        // clean up parameters to log
+        $params = $_GET;
+        if(isset($params['do'])) unset($params['do']);
+        if(isset($params['id'])) unset($params['id']);
+
+        // decide on virtual views
         if($ACT == 'search') {
-            $view = '~search/?' . rawurlencode($QUERY);
+            $view = '~search/';
+            $params['q'] = $QUERY;
         } elseif($ACT == 'admin') {
             $page = $INPUT->str('page');
             $view = '~admin';
             if($page) $view .= '/' . $page;
+            if(isset($params['page'])) unset($params['page']);
         } else {
             $view = str_replace(':', '/', $ID); // slashes needed for Content Drilldown
         }
-        $view = DOKU_REL . $view; // prepend basedir, allows logging multiple dir based animals in one tracker
 
-        $JSINFO['ga'] = array(
-            'trackingId' => $trackingId,
-            'anonymizeIp' => (bool) $this->getConf('anonymize'),
-            'action' => act_clean($ACT),
-            'trackOutboundLinks' => (bool) $this->getConf('track_links'),
-            'options' => $options,
-            'pageview' => $view
-        );
+        // prepend basedir, allows logging multiple dir based animals in one tracker
+        $view = DOKU_REL . $view;
+
+        // append query parameters
+        $query = http_build_query($params, '', '&');
+        if($query) $view .= '?' . $query;
+
+        return $view;
     }
 }
